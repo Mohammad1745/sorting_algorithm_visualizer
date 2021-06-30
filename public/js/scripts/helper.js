5bd7b4490b670dbd1612fbddf5dc4a068068fade
Mohammad Ali <mdali2016.227@gmail.com>
2021-06-30 13:15:27 +0600

    [Add] switch and shift methods for array

--- public/js/scripts/helper.js
+++ public/js/scripts/helper.js
@@ -6,6 +6,29 @@ function generateArray(length = 5) {
     return array
 }
 
+function switchElement (array, index1, index2) {
+    let temp = array[index1]
+    array[index1] = array[index2]
+    array[index2] = temp
+}
+
+function shiftElement (array, index1, index2) {
+    if (index2>index1){
+        let temp = array[index2]
+        for (let i = index2; i > index1; i--) {
+            array[i] = array[i - 1]
+        }
+        array[index1] = temp
+    }
+    else if(index1>index2+1) {
+        let temp = array[index1]
+        for (let i = index1; i > index2; i--) {
+            array[i] = array[i - 1]
+        }
+        array[index2] = temp
+    }
+}
+
 Array.prototype.equals = function(arr2) {
     return (
         this.length === arr2.length &&
